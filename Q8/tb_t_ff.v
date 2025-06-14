3e7447ead65b8792dc81421899c1b7b7d4830648
SidhanthKrishnan <f20231114@pilani.bits-pilani.ac.in>
2025-06-14 21:47:20 +0530

    tb_t_ff.v

--- Q8/tb_t_ff.v
+++ Q8/tb_t_ff.v
@@ -2,21 +2,19 @@
 module tb_T_ff();
   reg t, clk, pr, cr;
   wire q, q_bar;
+
   T_ff uut(.t(t), .clk(clk), .pr(pr), .cr(cr), .q(q), .q_bar(q_bar));
+
   initial begin
-    clk=0;
-    t=0;
-    pr=0; cr=1; #2;
-    pr=0; cr=0; #2;
-    t=1;
-    #5;
-    t=0;
-    #5;
-    t=1;
-    #5;
+    clk = 0;
+    t = 0;
+    pr = 1; cr = 0; #2;
+    cr = 1; #2;
+    t = 1; #10;
+    t = 0; #10;
+    t = 1; #10;
     $finish;
   end
 
- 
   always #5 clk = ~clk;
 endmodule

a75b29d417502f15d9607a34360c6c681d69b123
SidhanthKrishnan <f20231114@pilani.bits-pilani.ac.in>
2025-06-15 01:21:21 +0530

    Update tb_mod10_async.v

--- Q9/tb_mod10_async.v
+++ Q9/tb_mod10_async.v
@@ -1,12 +1,18 @@
 `timescale 1ns / 1ps
 module tb_mod10_async_counter();
-  reg clk, reset;
+  reg clk;
+  reg r;
   wire [3:0] count;
-  mod10_async_counter uut(.clk(clk), .reset(reset), .count(count));
+
+  mod10_async_counter uut(.clk1(clk), .r(r), .count(count));
+
   initial begin
-    clk = 0; reset = 1; #5;
-    reset = 0; #100;
+    clk = 0;
+    r = 0; #5;
+    r = 1;
+    #200;
     $finish;
   end
+
   always #5 clk = ~clk;
 endmodule

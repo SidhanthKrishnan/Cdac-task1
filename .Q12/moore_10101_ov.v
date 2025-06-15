d354911016809de72c18f42988d7c51e7ca2b2e9
SidhanthKrishnan <f20231114@pilani.bits-pilani.ac.in>
2025-06-16 02:33:26 +0530

    Update moore_10101_ov.v

--- .Q12/moore_10101_ov.v
+++ .Q12/moore_10101_ov.v
@@ -1,21 +1,26 @@
-`timescale 1ns / 1ps
 module moore_10101_ov(input clk, input rst, input in, output reg out);
   reg [2:0] state, next;
-  parameter S0=0, S1=1, S2=2, S3=3, S4=4, S5=5;
+  parameter S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5;
+
   always @(posedge clk or posedge rst) begin
-    if (rst) state <= S0;
-    else state <= next;
+    if (rst)
+      state <= S0;
+    else
+      state <= next;
   end
+
   always @(*) begin
     case (state)
       S0: next = in ? S1 : S0;
       S1: next = in ? S1 : S2;
       S2: next = in ? S3 : S0;
-      S3: next = in ? S4 : S2;
+      S3: next = in ? S1 : S4;
       S4: next = in ? S5 : S0;
-      S5: next = in ? S1 : S0;
+      S5: next = in ? S1 : S2; 
+      default: next = S0;
     endcase
   end
+
   always @(*) begin
     out = (state == S5);
   end
